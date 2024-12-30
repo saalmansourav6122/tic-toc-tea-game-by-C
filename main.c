6ce32cecb9e3a9747317ce7b6aefe4d4802d5c4e
saalmansourav6122 <saalmansourav111@gmail.com>
2024-12-30 13:08:43 +0600

    Its normal C program Tic Tac Toe game

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 void showBoard();
+int checkWin();
+
 char board[]={'0','1','2','3','4','5','6','7','8','9'};
 
 void main(){
@@ -19,6 +21,16 @@ void main(){
     }
     board[input] = mark;
     showBoard();
+
+    int result = checkWin();
+
+    if(result ==1){
+        printf("Player %d winner", player);
+        return;
+    }else if(result==0){
+        printf("draw");
+        return;
+    }
     player++;
     }
 }
@@ -27,7 +39,7 @@ void showBoard(){
     printf("\n\n");
     printf("!!! Tic Tac Toe Game !!!!\n\n");
     printf("    |    |    \n");
-    printf("  %c | %c  | %c  \n", board[1],board[2],board[3]);
+    printf("  %c |  %c | %c \n", board[1],board[2],board[3]);
     printf("____|____|____\n");
     printf("    |    |    \n");
     printf("  %c |  %c |  %c \n", board[4],board[5],board[6]);
@@ -35,4 +47,42 @@ void showBoard(){
     printf("    |    |    \n");
     printf("  %c |  %c |  %c \n", board[7],board[8],board[9]);
     printf("    |    |    \n");
+}
+
+int checkWin(){
+if(board[1]==board[2] && board[2]==board[3]){
+    return 1;
+}
+if(board[1]==board[4] && board[4]==board[7]){
+    return 1;
+}
+if(board[7]==board[8] && board[8]==board[9]){
+    return 1;
+}
+if(board[3]==board[6] && board[6]==board[9]){
+    return 1;
+}
+if(board[1]==board[5] && board[5]==board[9]){
+    return 1;
+}
+if(board[3]==board[5] && board[5]==board[7]){
+    return 1;
+}
+if(board[2]==board[5] && board[5]==board[8]){
+    return 1;
+}
+if(board[4]==board[5] && board[5]==board[6]){
+    return 1;
+}
+int count = 0;
+for (int i = 1; i<=9; i++){
+    if(board[i]=='X' || board[i]=='O'){
+        count++;
+    }
+}
+if(count==9){
+    return 0;
+}
+return -1;
+
 }
